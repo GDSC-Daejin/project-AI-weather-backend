3cb3f2c08d8160db6a64af4161255f6438c9c523
dntjd1097 <dntjd1097@gmail.com>
2023-05-31 19:10:03 +0900

    fix : bug

--- db-insert/csv_reader.py
+++ db-insert/csv_reader.py
@@ -2,7 +2,7 @@ import csv
 import glob
 import multiprocessing
 import os
-from db import sql_command
+from db import sql_command, get_database_connection
 
 
 def mt_process(args):
@@ -25,25 +25,25 @@ if __name__ == "__main__":
     file_list = glob.glob(file_pattern)
 
     process = 10
-
-    for file_path in file_list:
-        with open(file_path, "r") as f:
-            rdr = csv.reader(f)
-            next(rdr)
-            with multiprocessing.Pool(processes=process) as pool:
-                for line in rdr:
-                    Label = {}
-                    Label["index"] = line[0]
-                    Label["name"] = line[1]
-                    Label["cg1"] = line[2]
-                    Label["cg2"] = line[3]
-                    Label["url"] = line[4]
-                    Label["src"] = line[5]
-                    Label["fit"] = line[6]
-                    Label["seasons"] = line[7]
-                    Label["thickness"] = line[8]
-                    pool.apply_async(mt_process, (Label,))
-                pool.close()
-                pool.join()
-        # if os.path.isfile(file_path):
-        #     os.remove(file_path)
+    if get_database_connection():
+        for file_path in file_list:
+            with open(file_path, "r", encoding="UTF8") as f:
+                rdr = csv.reader(f)
+                next(rdr)
+                with multiprocessing.Pool(processes=process) as pool:
+                    for line in rdr:
+                        Label = {}
+                        Label["index"] = line[0]
+                        Label["name"] = line[1]
+                        Label["cg1"] = line[2]
+                        Label["cg2"] = line[3]
+                        Label["url"] = line[4]
+                        Label["src"] = line[5]
+                        Label["fit"] = line[6]
+                        Label["seasons"] = line[7]
+                        Label["thickness"] = line[8]
+                        pool.apply_async(mt_process, (Label,))
+                    pool.close()
+                    pool.join()
+            if os.path.isfile(file_path):
+                os.remove(file_path)
--- db-insert/db.py
+++ db-insert/db.py
@@ -9,7 +9,8 @@ def sql_command(name, cate, sub_cate, url, img, style, seasons, thick, idx):
     db_insert(sql, values, idx)
 
 
-def db_insert(sql, values, idx):
+def get_database_connection():
+    global connection
     try:
         DB_IP_addr = os.environ["DB_IP_ADDR"]
     except KeyError:
@@ -18,29 +19,24 @@ def db_insert(sql, values, idx):
     try:
         # Connect to the database
         connection = pymysql.connect(
-            host=DB_IP_addr,
-            user="james",
-            password="password",
-            db="ai_weather"
+            host=DB_IP_addr, user="james", password="password", db="ai_weather"
         )
-
-        print("Database connection successful.")
-
-        with connection.cursor() as cursor:
-            try:
-                # Execute the SQL query
-                cursor.execute(sql, values)
-            except Exception as e:
-                print(idx, e)
-                pass
-
-        # Commit the changes and close the connection
-        connection.commit()
-        connection.close()
-
-        print("Data inserted successfully.")
+        return connection
     except pymysql.err.OperationalError as e:
         print("Failed to connect to the database:", e)
         raise Exception("Failed to connect to the database:", e)
 
 
+def db_insert(sql, values, idx):
+    connection = get_database_connection()
+    with connection.cursor() as cursor:
+        try:
+            # Execute the SQL query
+            cursor.execute(sql, values)
+        except Exception as e:
+            print(idx, e)
+            pass
+
+    # Commit the changes and close the connection
+    connection.commit()
+    connection.close()
